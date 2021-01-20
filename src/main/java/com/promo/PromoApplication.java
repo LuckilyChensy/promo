f8ec4ebdde7aef7c4ab76dff58e004bc0b6fd41f
SoSo <LuckilyChensy@163.com>
2021-01-21 00:22:11 +0800

    秒杀系统 - Application init

--- src/main/java/com/promo/PromoApplication.java
+++ src/main/java/com/promo/PromoApplication.java
@@ -3,6 +3,8 @@ package com.promo;
 import com.promo.dao.UserDOMapper;
 import com.promo.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,27 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication(scanBasePackages = {"com.promo"})
 @RestController
 @MapperScan("com.promo.dao")
-public class App {
+public class PromoApplication {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
-    public String home() {
+    public String test() {
+
         UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+
         if (userDO == null) {
-            return "用户对象不存在";
+            return "user not exists";
         } else {
-            return "姓名: "+userDO.getName()
+            return    "<h2>"
+                    + "姓名: "+userDO.getName()
                     +" 年纪: "+userDO.getAge()
                     +" 注册渠道: "+userDO.getRegisitMode()
                     +" 电话号码: "+userDO.getTelphone()
-                    +" 性别: "+userDO.getGender();
+                    +" 性别: "+userDO.getGender()
+                    + "</h2>";
         }
     }
 
     public static void main( String[] args ){
-        System.out.println( "promo project bootStrap!" );
-        SpringApplication.run(App.class);
+        SpringApplication.run(PromoApplication.class);
     }
+
 }
