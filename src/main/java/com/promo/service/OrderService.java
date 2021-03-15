82dbdd86c1090105e1cd03a554fbbc9f03616674
SoSo <LuckilyChensy@163.com>
2021-03-15 23:03:31 +0800

    秒杀系统 - 交易服务 - 代码重构

--- src/main/java/com/promo/service/OrderService.java
+++ src/main/java/com/promo/service/OrderService.java
@@ -4,5 +4,6 @@ import com.promo.error.BusinessException;
 import com.promo.service.model.OrderModel;
 
 public interface OrderService {
+    // 提供指定用户与商品，活动，库存之间的关系
     OrderModel createOrder(Integer userId,Integer itemId,Integer promoId,Integer amount,String stockLogId) throws BusinessException;
 }
